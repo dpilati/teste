988ad92d112ea98401ccfa7788fb6a8a76a9b477
dpilati <50930130+dpilati@users.noreply.github.com>
2022-06-07 15:07:48 -0300

    Create __init__.py

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,27 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('productId')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('productId')
+
+    if name:
+        if name == '75542e38-563f-436f-adeb-f426f1dabb5c':
+            return func.HttpResponse(f"The product name for your product id {name} is Starfruit Explosion and the description is This starfruit ice cream is out of this world!")
+        else:
+            return func.HttpResponse(f"The product not found")
+    else:
+        return func.HttpResponse(
+             "Product ID not found",
+             status_code=200
+        )
